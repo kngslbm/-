c962143256d73fa91b0f8471bbfedeea9918c992
kngslbm <kangseulbeom@gmail.com>
2024-09-08 07:21:34 +0900

    BE_feat : JWT 제공 로그아웃 url 추가

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView,
 )
 from .views import *
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
     path('signin/', TokenObtainPairView.as_view(), name="signin"),
+    path("signout/", TokenBlacklistView.as_view(), name="signout"),
     path('token/refresh/', TokenRefreshView.as_view()),
     path("change-password/", ChangePasswordAPIView.as_view()),
     path("<str:username>/", UserDetailAPIView.as_view()),
