9152de03950e22f1d1b2f65889be7dd12cfbcabf
kngslbm <kangseulbeom@gmail.com>
2024-09-08 20:29:12 +0900

    BE_feat : django 관리자 페이지에 User 모델과 JWT blacklist 추가

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+
+admin.site.unregister(OutstandingToken)
+admin.site.register(User)
